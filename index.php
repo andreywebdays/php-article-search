afc8f7d84fc07e355c3f978b7735fc02733b517e
Andrey <AndreyWebDays@gmail.com>
2022-03-13 11:01:04 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once 'header.php';
+?>
+    
+<form action="search.php" method="POST">
+    <input type="text" name="search" placeholder="Search">
+    <button type="submit" name="submit-search">Search</button>
+</form>
+
+</body>
+</html>
